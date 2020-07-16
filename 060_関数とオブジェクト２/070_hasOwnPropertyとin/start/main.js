859397babfa4bb1727b00b0ca439b740d800bcfe
tkm <sumikawa.web@gmail.com>
2020-07-16 14:16:07 +0900

    prototype

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -1,10 +1,14 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
-
-Object.prototype.hello = function() {
-    console.log('Object: hello ' + this.name);
-}
+// Person.prototype.gender = 12;
+Object.prototype.hello = function () {
+  console.log('Object: hello ' + this.name);
+};
 
 const bob = new Person('Bob', 18);
+console.log(bob);
+const result = bob.hasOwnProperty('hello');
+console.log(result);
+console.log('hello' in bob);
--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -3,6 +3,19 @@ function Person(name, age) {
   this.age = age;
 }
 
-Person.prototype.hello = function() {
+Person.prototype.hello = function () {
   console.log('hello ' + this.name);
+};
+
+function Japanese(name, age, gender) {
+  Person.call(this, name, age);
+  this.gender = gender;
 }
+Japanese.prototype = Object.create(Person.prototype);
+Japanese.prototype.hello = function () {
+  console.log('こんにちは' + this.name);
+};
+
+const taro = new Japanese('taro', 23, 'male');
+console.log(taro);
+taro.hello();
