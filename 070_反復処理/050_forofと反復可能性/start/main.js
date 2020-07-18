49936f17ffbd2e61c4a750013d8a5d162f690a16
tkm <sumikawa.web@gmail.com>
2020-07-18 20:25:50 +0900

    map set

--- 070_反復処理/050_forofと反復可能性/start/main.js
+++ 070_反復処理/050_forofと反復可能性/start/main.js
@@ -1,5 +1,21 @@
 const obj = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3'
+  prop1: 'value1',
+  prop2: 'value2',
+  prop3: 'value3',
+};
+Object.prototype.method = function () { };
+Object.defineProperty(obj, 'prop4', {
+	enumerable: false
+};
+const s = Symbol();
+obj[s] = 'symbol';
+
+const items = Object.entries(obj);
+// const items = Object.keys(obj);
+// const items = Object.values(obj);
+
+console.log(items);
+
+for (let [k, v] of items) {
+  console.log(k, v);
 }
