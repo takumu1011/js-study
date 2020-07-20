46f6ec8f25cd1eac93b58c349b4356cef72d8541
tkm <sumikawa.web@gmail.com>
2020-07-20 20:44:29 +0900

    generator

---
+++ 070_反復処理/090_ジェネレーター/start/main.js
@@ -0,0 +1,44 @@
+// function* gen() {
+//   yield 1;
+//   yield 2;
+//   return 3;
+// }
+
+// const it = gen();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+// function* genIterator(max = 10) {
+//   let i = 0;
+//   while (i < max) {
+//     yield i++;
+//   }
+//   return;
+// }
+
+// const it = genIterator(10);
+// let a = it.next();
+// while (!a.done) {
+//   a = it.next();
+// }
+// const obj = {
+//   [Symbol.iterator]: genIterator,
+// };
+
+// for (let i of genIterator()) {
+//   console.log(i);
+// }
+
+const obj = {
+  *[Symbol.iterator](max = 10) {
+    let i = 0;
+    while (i < max) {
+      yield i++;
+    }
+    return;
+  },
+};
+for (let i of obj) {
+  console.log(i);
+}
