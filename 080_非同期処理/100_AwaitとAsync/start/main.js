2aea4e9209b20b36d3305cc610c020182bc09758
tkm <sumikawa.web@gmail.com>
2020-07-24 17:46:54 +0900

    fetch

--- 080_非同期処理/100_AwaitとAsync/start/main.js
+++ 080_非同期処理/100_AwaitとAsync/start/main.js
@@ -1,20 +1,38 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
       console.log(val++);
       resolve(val);
     }, 1000);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  return val;
+}
+
+init().then(function (val) {
+  console.log('hello' + val);
+});
+
+// sleep(0)
+//   .then(function (val) {
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   });
--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,11 +1,36 @@
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  if (response.ok) {
+    const json = await response.json();
+    if (!json.length) {
+      throw new NoDataError('no data found');
+    }
+    return json;
+  }
+}
 
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NoDataError';
+  }
+}
 
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
-// }
+async function init() {
+  try {
+    const users = await fetchUsers();
+    for (const user of json) {
+      console.log(`I'm ${user.name}, ${user.age} years old`);
+    }
+  } catch (e) {
+    if (e instanceof NoDataError) {
+      console.log(e);
+    } else {
+      console.error(e);
+    }
+  } finally {
+    console.log('bye');
+  }
+}
 
-// fetchUsers();
+init();
