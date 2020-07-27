3a438617d34bc93e8743bb83590c7d59c4bad112
tkm <sumikawa.web@gmail.com>
2020-07-27 21:47:17 +0900

    proxy

--- 070_反復処理/060_MapとSet/start/main.js
+++ 070_反復処理/060_MapとSet/start/main.js
@@ -1,33 +1,38 @@
 const map = new Map();
 const key1 = {};
-map.set(key1, 'value1');
 
+map.set(key1, 'value1');
 console.log(map.get(key1));
 
 const key2 = function () {};
 map.set(key2, 'value2');
 console.log(map.get(key2));
 
+// let key3 = 0;
 let key3;
 map.set((key3 = 0), 'value3');
-console.log(map.get(key3));
-
-map.delete(key3);
+// console.log(map.get(key3));
 console.log(map.get(0));
+
+// map.delete(key3);
+// console.log(map.get(0));
 
 for (const [k, v] of map) {
   console.log(k, v);
 }
 
+console.log(map);
+
 const s = new Set();
 s.add(key1);
 s.add(key2);
 s.add(key3);
-s.delete(key3);
 
-console.log(s.has(key3));
-const arry = Array.from(s);
-console.log(arry);
+console.log(s.has(key2));
+// const arry = Array.from(s);
+const arry = [...s];
+console.log('配列' + arry);
+
 for (let k of s) {
   console.log(k);
 }
--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,26 @@
 const targetObj = { a: 0 };
+const handler = {
+  set: function (target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`);
+    // target[prop] = value;
+    throw new Error('cannot add prop');
+  },
+  get: function (target, prop, receiver) {
+    if (target.hasOwnProperty(prop)) {
+      return target[prop];
+    } else {
+      return '-1';
+    }
+    console.log(`[get]: ${prop}`);
+    return target[prop];
+  },
+  deleteProperty: function (target, prop) {
+    console.log(`[delete]: ${prop}`);
+    delete target[prop];
+  },
+};
+const pxy = new Proxy(targetObj, handler);
+// pxy.a = 1;
+// pxy.a;
+console.log(pxy.b);
+delete pxy.a;
